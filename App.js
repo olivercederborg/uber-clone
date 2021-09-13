34dc3d62e079bf8ee3ed22c60dd6ff00b604345b
Oliver Cederborg <hi@olivercederborg.com>
2021-09-14 01:38:06 +0200

    feat: add HomeScreen + MapScreen

--- App.js
+++ App.js
@@ -1,21 +1,43 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React from 'react'
+import { KeyboardAvoidingView } from 'react-native'
+import { Provider } from 'react-redux'
+import HomeScreen from './screens/HomeScreen'
+import MapScreen from './screens/MapScreen'
+import { store } from './store'
+import { SafeAreaProvider } from 'react-native-safe-area-context'
+import { NavigationContainer } from '@react-navigation/native'
+import { createNativeStackNavigator } from '@react-navigation/native-stack'
+
+const Stack = createNativeStackNavigator()
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+	return (
+		<Provider store={store}>
+			<NavigationContainer>
+				<SafeAreaProvider>
+					<KeyboardAvoidingView
+						behavior={Platform.OS === 'ios' ? 'padding' : 'height'}
+						keyboardVerticalOffset={Platform.OS === 'ios' ? -64 : 0}
+						style={{ flex: 1 }}>
+						<Stack.Navigator>
+							<Stack.Screen
+								name='HomeScreen'
+								component={HomeScreen}
+								options={{
+									headerShown: false
+								}}
+							/>
+							<Stack.Screen
+								name='MapScreen'
+								component={MapScreen}
+								options={{
+									headerShown: false
+								}}
+							/>
+						</Stack.Navigator>
+					</KeyboardAvoidingView>
+				</SafeAreaProvider>
+			</NavigationContainer>
+		</Provider>
+	)
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
