f987c26a2ac9fd4d9b12ece196b780bf83ea9208
Oliver Cederborg <hi@olivercederborg.com>
2021-09-17 05:48:34 +0200

    feat: add "back to menu" button

--- screens/MapScreen.js
+++ screens/MapScreen.js
@@ -1,17 +1,26 @@
 import React from 'react'
-import { View, Text } from 'react-native'
+import { View, Text, TouchableOpacity } from 'react-native'
 import tw from 'tailwind-react-native-classnames'
 import Map from '../components/Map'
 import { createNativeStackNavigator } from '@react-navigation/native-stack'
 import NavigateCard from '../components/NavigateCard'
 import RideOptionsCard from '../components/RideOptionsCard'
+import { Icon } from 'react-native-elements'
+import { useNavigation } from '@react-navigation/core'
 
 const MapScreen = () => {
 	const Stack = createNativeStackNavigator()
+	const navigation = useNavigation()
 	return (
 		<View>
-			<View style={tw`h-2/5`}>
+			<View style={tw`h-2/5 relative`}>
 				<Map />
+
+				<TouchableOpacity
+					onPress={() => navigation.navigate('HomeScreen')}
+					style={tw`p-4 absolute top-12 left-6 bg-white rounded-full`}>
+					<Icon name='menu' type='feather' />
+				</TouchableOpacity>
 			</View>
 
 			<View style={tw`h-3/5`}>
